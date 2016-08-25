cafe220153357125c0ae6c16ea6bf26af3c9dda4
Will Leinweber <will@bitfission.com>
2016-08-24 18:38:07 -0700

    increase speed from 3k/sec to 22k/second

--- src/hitcounter.cr
+++ src/hitcounter.cr
@@ -27,20 +27,41 @@ class IPTracker
   end
 end
 
-def nextval
-  DB.exec({Int64}, "select nextval('hitcounter')").rows.first.first
-end
+class Counter
+  getter count : Int64
 
-def currval
-  DB.exec({Int64}, "select currval('hitcounter')").rows.first.first
+  def initialize
+    @count = DB.exec({Int64}, "select count from hits limit 1").rows.first.first
+    @m = Mutex.new
+    async_save_loop
+  end
+
+  def inc
+    @m.synchronize { @count += 1 }
+    @count
+  end
+
+  private def save
+    DB.exec("update hits set count = $1", [count])
+  end
+
+  private def async_save_loop
+    spawn do
+      loop do
+        sleep 1
+        save
+      end
+    end
+  end
 end
 
 tracker = IPTracker.new
+counter = Counter.new
 
 port = (ENV["PORT"]? || 8080).to_i
 server = HTTP::Server.new("0.0.0.0", port) do |context|
   ip = context.request.headers["X-Forwarded-For"]?
-  count = tracker.check(ip) ? nextval : currval
+  count = tracker.check(ip) ? counter.inc : counter.count
   res = context.response
   res.content_type = "application/json"
   #  res.headers.add("Access-Control-Allow-Origin", "*")
